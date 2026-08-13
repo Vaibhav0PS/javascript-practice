bc863c64d71a7840804294d2ba8cce276f94eeaa
Vaibhav Patole <patolevaibhav77@gmail.com>
2026-08-13 16:44:05 +0000

    variables basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 12412538;
+let accountName = "Vaibhav Patole";
+var accountEmail = "abc@gmail.com";
+let accountCity;
+
+console.table([accountId,accountName,accountEmail,accountCity])
