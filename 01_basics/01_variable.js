b72c2d5b9edbea294ced5609fe6b8dc658503c79
Vaibhav Patole <patolevaibhav77@gmail.com>
2026-08-13 16:48:44 +0000

    added comments

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,4 +3,14 @@ let accountName = "Vaibhav Patole";
 var accountEmail = "abc@gmail.com";
 let accountCity;
 
+// dont use var for declaring variables is out of scope 
+// "const" makes value constant of that variable means you cannot change the value of that particular variable 
+// "let" keyword can make values change also the var but we didnt use that 
+
+// accountId = 12412345; this will throw error
+
+
+accountName = "Kiran Karjule";
+accountEmail = "kiran@outlook.com"
+
 console.table([accountId,accountName,accountEmail,accountCity])
