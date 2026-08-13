297989842ac85442d88466d210c9b371c88af518
Vaibhav Patole <patolevaibhav77@gmail.com>
2026-08-13 17:15:35 +0000

    basics of data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+let number = 1234
+let string = "demon"
+let isMissing = false
+let firstValue = null
+
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine
+// symbol => unique
+// object
+
+console.log(typeof number) // number
+console.log(typeof string) // string
+console.log(typeof isMissing) // boolean
+console.log(typeof firstValue) // object
+console.log(typeof undefined) // undefine
+console.log(typeof null) // object
+
+
